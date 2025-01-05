24f1d83a3e25311d27888afa529ee5167af6f54e
Ankur Verma <ankurverma7619@gmail.com>
2025-01-05 16:53:04 +0530

    Data Types in TS

--- TypeScript-Basics/app.js
+++ TypeScript-Basics/app.js
@@ -4,3 +4,12 @@ num = 10000;
 var str = "Hello, world!"; //we have to initialize immediately and can not reassign it later
 console.log(num, str);
 console.log("Learning TypeScript");
+function sum(num1, num2) {
+    return num1 + num2;
+}
+//can assign types to variables also
+var num3 = 100;
+console.log(num3, typeof (num3));
+var num4 = 10;
+console.log(sum(10, 20));
+console.log(sum(20, 20)); //concatenate the string values
--- TypeScript-Basics/app.ts
+++ TypeScript-Basics/app.ts
@@ -5,3 +5,19 @@ const str="Hello, world!" //we have to initialize immediately and can not reassi
 
 console.log(num,str);
 console.log("Learning TypeScript")
+
+
+function sum(num1: number,num2:number){
+return num1 + num2;
+}
+
+//can assign types to variables also
+let num3:number = 100
+console.log(num3,typeof(num3));
+
+let num4=10
+
+
+
+console.log(sum(10,20));
+console.log(sum(20,20)); //concatenate the string values
