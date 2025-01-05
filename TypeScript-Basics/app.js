02882f2328460ac8e4bab2facea6a8fcdb1238c2
Ankur Verma <ankurverma7619@gmail.com>
2025-01-05 15:33:54 +0530

    Basics of TS

---
+++ TypeScript-Basics/app.js
@@ -0,0 +1,6 @@
+// Learning variables
+var num = 100; //when we create a variable with let keyword we don't need to initialize it immediately we can do later
+num = 10000;
+var str = "Hello, world!"; //we have to initialize immediately and can not reassign it later
+console.log(num, str);
+console.log("Learning TypeScript");
---
+++ TypeScript-Basics/app.ts
@@ -0,0 +1,7 @@
+// Learning variables
+let num=100 //when we create a variable with let keyword we don't need to initialize it immediately we can do later
+num=10000
+const str="Hello, world!" //we have to initialize immediately and can not reassign it later
+
+console.log(num,str);
+console.log("Learning TypeScript")
